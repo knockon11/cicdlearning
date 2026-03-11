1236bad468bb5448b55bc1091e5eda7669341b02
knockon11 <abdullahnegm657@gmail.com>
2026-03-11 04:03:41 +0100

    First Cypress Automation with CI/CD

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe('SauceDemo Login Suite', () => {
+    it('Should login and see the inventory page', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
+        // التأكد إننا دخلنا صح
+        cy.url().should('include', '/inventory.html')
+        cy.get('.title').should('have.text', 'Products')
+    })
+})
